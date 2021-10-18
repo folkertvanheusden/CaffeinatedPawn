8c4cdf552bf6e47b1062697c2bfacaab4223529a
folkert van heusden <mail@vanheusden.com>
2021-10-18 16:16:36 +0200

    Release script - for internal use

---
+++ caffeinatedpawn/release.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+git checkout master
+
+mvn package
+
+DIR=C.P.
+rm -f $DIR
+mkdir $DIR
+
+GITHASH=`git rev-parse --short HEAD`
+
+cp target/CaffeinatedPawn-1.0.jar ${DIR}/CaffeinatedPawn-${GITHASH}.jar
+cp ../chesslib/target/chesslib-1.3.2.jar ${DIR}/
+
+WRAPPER=${DIR}/wrapper.sh
+echo '#! /bin/sh' > ${WRAPPER}
+echo "java -cp CaffeinatedPawn-${GITHASH}.jar:chesslib-1.3.2.jar com.vanheusden.CaffeinatedPawn.CaffeinatedPawn" >> ${WRAPPER}
+chmod +x ${WRAPPER}
+
+cp ../README.md ${DIR}
+
+ZIP=CaffeinatedPawn-${GITHASH}.zip
+zip -9vr ${ZIP} ${DIR}
+
+scp -C ${ZIP} @belle:site/CaffeinatedPawn/
+
+rm -rf ${ZIP} ${DIR}
